93515393730249afd314066cd2d33e32be55d926
Johan Hanses <johanhanses@gmail.com>
2023-08-05 20:03:40 +0200

    clean up

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -172,6 +172,26 @@ use({
 --   end
 -- })
 
+-- Git integration.
+use({
+  'lewis6991/gitsigns.nvim',
+  config = function()
+    require('gitsigns').setup()
+    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
+    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+    vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
+    vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
+    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
+  end,
+})
+
+-- Git commands.
+use({
+  'tpope/vim-fugitive',
+  requires = 'tpope/vim-rhubarb',
+})
+
 if packer_bootstrap then
   require('packer').sync()
 end
