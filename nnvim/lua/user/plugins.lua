d718fda5098d7255723bb3a54801f1e8e03a821f
Johan Hanses <johanhanses@gmail.com>
2023-08-07 18:08:13 +0200

    reconstruct wip

--- nnvim/lua/user/plugins.lua
+++ nnvim/lua/user/plugins.lua
@@ -26,32 +26,37 @@ local use = require('packer').use
 
 use('wbthomason/packer.nvim')
 
+-- use({
+--   'jessarcher/onedark.nvim',
+--   config = function()
+--     vim.cmd('colorscheme onedark')
+
+--     -- Hide the characters in FloatBorder
+--     vim.api.nvim_set_hl(0, 'FloatBorder', {
+--       fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+--       bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+--     })
+
+--     -- Make the StatusLineNonText background the same as StatusLine
+--     vim.api.nvim_set_hl(0, 'StatusLineNonText', {
+--       fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
+--       bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
+--     })
+
+--     -- Hide the characters in CursorLineBg
+--     vim.api.nvim_set_hl(0, 'CursorLineBg', {
+--       fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+--       bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+--     })
+
+--     vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#4d505e' })
+--     vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#4d505e' })
+--   end,
+-- })
+
 use({
-  'jessarcher/onedark.nvim',
-  config = function()
-    vim.cmd('colorscheme onedark')
-
-    -- Hide the characters in FloatBorder
-    vim.api.nvim_set_hl(0, 'FloatBorder', {
-      fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-      bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
-    })
-
-    -- Make the StatusLineNonText background the same as StatusLine
-    vim.api.nvim_set_hl(0, 'StatusLineNonText', {
-      fg = vim.api.nvim_get_hl_by_name('NonText', true).foreground,
-      bg = vim.api.nvim_get_hl_by_name('StatusLine', true).background,
-    })
-
-    -- Hide the characters in CursorLineBg
-    vim.api.nvim_set_hl(0, 'CursorLineBg', {
-      fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-      bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
-    })
-
-    vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#4d505e' })
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { fg = '#4d505e' })
-  end,
+  'svrana/neosolarized.nvim',
+  requires = { 'tjdevries/colorbuddy.nvim' }
 })
 
 use({
@@ -150,10 +155,10 @@ use({
 use({
   'akinsho/bufferline.nvim',
   requires = 'kyazdani42/nvim-web-devicons',
-  after = 'onedark.nvim',
-  config = function()
-    require('user/plugins/bufferline')
-  end,
+  -- after = 'onedark.nvim',
+  -- config = function()
+  --   require('user/plugins/bufferline')
+  -- end,
 })
 
 -- Display indentation lines.
