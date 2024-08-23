dda4f366faaa6356f96a484152b49b4ce68bcbe7
Johan Hanses <johanhanses@gmail.com>
2024-08-23 19:00:15 +0200

    updates

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -33,7 +33,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   -- install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = {
+    enabled = true, -- automatically check for plugin updates
+    notify = false, -- get a notification when new updates are found
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
